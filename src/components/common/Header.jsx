7d438227b74afe2fac3221171e50bfebcf0079e2
Janella Web <quizonwebsites@gmail.com>
2026-05-17 19:34:06 -0600

    Connected App component with Header and ProductsPage

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Janella Apparel Market</h1>
+      <p>Your favorite apparel and fashion essentials.</p>
+    </header>
+  )
+}
+
+export default Header
